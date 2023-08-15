a8588bd57ee00683b463aef43b6766e9cbeea17b
Aadi Poddar <aadipoddarmail@gmail.com>
2023-08-15 19:37:44 +0530

    Basic Get X State Controller

--- lib/common/routes/pages.dart
+++ lib/common/routes/pages.dart
@@ -1,4 +1,4 @@
-
+import 'package:chatty/pages/frame/welcome/index.dart';
 import 'package:flutter/material.dart';
 import 'package:chatty/common/middlewares/middlewares.dart';
 
@@ -10,14 +10,16 @@ class AppPages {
   static const INITIAL = AppRoutes.INITIAL;
   static final RouteObserver<Route> observer = RouteObservers();
   static List<String> history = [];
- /*
+
   static final List<GetPage> routes = [
-    // 免登陆
+    // about boot up the app
     GetPage(
       name: AppRoutes.INITIAL,
-      page: () => WelcomePage(),
+      page: () => const WelcomePage(),
       binding: WelcomeBinding(),
     ),
+
+    /*
     GetPage(
       name: AppRoutes.SIGN_IN,
       page: () => SignInPage(),
@@ -43,9 +45,20 @@ class AppPages {
     // 首页
     GetPage(name: AppRoutes.Contact, page: () => ContactPage(), binding: ContactBinding()),
     //消息
-    GetPage(name: AppRoutes.Message, page: () => MessagePage(), binding: MessageBinding(),middlewares: [
-       RouteAuthMiddleware(priority: 1),
-     ],),
+
+    */
+
+    // Message Page
+    // GetPage(
+    //   name: AppRoutes.Message,
+    //   page: () => MessagePage(),
+    //   binding: MessageBinding(),
+    //   middlewares: [
+    //     RouteAuthMiddleware(priority: 1),
+    //   ],
+    // ),
+
+    /*
     //我的
     GetPage(name: AppRoutes.Profile, page: () => ProfilePage(), binding: ProfileBinding()),
     //聊天详情
@@ -53,12 +66,6 @@ class AppPages {
 
     GetPage(name: AppRoutes.Photoimgview, page: () => PhotoImgViewPage(), binding: PhotoImgViewBinding()),
     GetPage(name: AppRoutes.VoiceCall, page: () => VoiceCallViewPage(), binding: VoiceCallViewBinding()),
-    GetPage(name: AppRoutes.VideoCall, page: () => VideoCallPage(), binding: VideoCallBinding()),
-  ];*/
-
-
-
-
-
-
+    GetPage(name: AppRoutes.VideoCall, page: () => VideoCallPage(), binding: VideoCallBinding()), */
+  ];
 }
